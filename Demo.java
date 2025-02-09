2c78cd5216b7dde5dcbaaf52f7d76a6a63f807ee
Gunasingha KGNN <141006999+Nipuna7@users.noreply.github.com>
2025-02-09 17:22:23 +0000

    initialize

---
+++ Demo.java
@@ -0,0 +1,17 @@
+
+class Car{
+    public void drive(){
+
+    }
+    public void playMusic(){
+        System.out.println("play music");
+    }
+}
+
+public class Demo{
+    public static void main(String[] args){
+        Car obj=new Car();
+        obj.drive();
+        obj.playMusic();
+    }
+}
