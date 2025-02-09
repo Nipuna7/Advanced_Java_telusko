f8e9542b1dc2b22f804ebb060ef40cd7cb1a106d
Gunasingha KGNN <141006999+Nipuna7@users.noreply.github.com>
2025-02-09 19:56:22 +0000

    iniialize

--- Demo.java
+++ Demo.java
@@ -1,17 +1,26 @@
 
-class Car{
-    public void drive(){
+abstract class Car{
+    public abstract void drive();
+    public abstract void Fly();
 
-    }
     public void playMusic(){
         System.out.println("play music");
+    }
+}
+class WagonR extends Car{
+    public void Fly(){
+        System.out.println("fly..");
+    }
+    public void drive(){
+        System.out.println("Driving..");
     }
 }
 
 public class Demo{
     public static void main(String[] args){
-        Car obj=new Car();
+        Car obj=new WagonR();
         obj.drive();
         obj.playMusic();
+        obj.Fly();
     }
 }
