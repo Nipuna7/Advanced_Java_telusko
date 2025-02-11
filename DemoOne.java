fab8847b9cf2c94375ef1af854314f9fa4be1804
Gunasingha KGNN <141006999+Nipuna7@users.noreply.github.com>
2025-02-11 14:56:58 +0000

    initialize

---
+++ DemoOne.java
@@ -0,0 +1,17 @@
+
+class A{
+   public void show(){
+    System.out.println("in A show");
+   }
+}
+class B extends A{
+    public void show(){
+        System.out.println("in B show");
+    }
+}
+
+public class DemoOne {
+    public static void main(String[] args) {
+        
+    }
+}
