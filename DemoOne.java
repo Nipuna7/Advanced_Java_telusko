0b06298d12915117cde07ad403c0a4b563534095
Gunasingha KGNN <141006999+Nipuna7@users.noreply.github.com>
2025-02-12 11:27:30 +0000

    initialize

---
+++ DemoOne.java
@@ -0,0 +1,14 @@
+public class DemoOne{
+    public static void main(String[] args) {
+        int i=0;
+        int j=0;
+
+        try{
+            j=18/i;
+        }
+        catch(Exception e){
+            System.out.println("Somthing went wrong");
+        }
+    }
+    
+}
